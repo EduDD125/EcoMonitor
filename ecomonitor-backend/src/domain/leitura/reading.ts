4d259404fa025e923b5d9da99c298a4862579f83
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-15 10:35:21 -0300

    feat: alterado nome e codigos da entidade Leitura para ingles, adicionado atributo id e abstracao de estrategia de designação de id para objetivos persistidos

---
+++ ecomonitor-backend/src/domain/leitura/reading.ts
@@ -0,0 +1,53 @@
+import { idStrategy } from "../../utils/idStrategy";
+
+export class Reading {
+  private id: string;
+  private location: string;
+  private dateTime: string;
+  private measurementType: string;
+  private value: string;
+
+  constructor(location: string, dateTime: string, measurementType: string, value: string, id?: string) {
+    this.id = id ?? idStrategy(); 
+    this.location = location;
+    this.dateTime = dateTime;
+    this.measurementType = measurementType;
+    this.value = value;
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  getLocation(): string {
+    return this.location;
+  }
+
+  setLocation(location: string): void {
+    this.location = location;
+  }
+
+  getDateTime(): string {
+    return this.dateTime;
+  }
+
+  setDateTime(dateTime: string): void {
+    this.dateTime = dateTime;
+  }
+
+  getMeasurementType(): string {
+    return this.measurementType;
+  }
+
+  setMeasurementType(measurementType: string): void {
+    this.measurementType = measurementType;
+  }
+
+  getValue(): string {
+    return this.value;
+  }
+
+  setValue(value: string): void {
+    this.value = value;
+  }
+}
