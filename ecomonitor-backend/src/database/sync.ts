1921ccae67c4f2fcce21ec7a90f2afcfcb58023b
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-16 20:41:52 -0300

    feat: add configuracao do BD com Sequelize e script de verificacao do banco
    o arquivo src/config/database.ts determina as configuracoes basica do banco de Dados em PostgreSQL usando o ORM Sequelize
    
    o arquivo src/database/sync.ts e um script paa garantir sincronizacao do banco de dados apos realizar vericacao da existencia das tabelas dos modelos definidos.

---
+++ ecomonitor-backend/src/database/sync.ts
@@ -0,0 +1,34 @@
+import { sequelize } from "../config/database";
+import { LogModel } from "../models/LogsModel";
+import { ReadingModel } from "../models/ReadingModel";
+
+const checkAndSyncDatabase  = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Database connected!");
+
+        const [results] = await sequelize.query(`
+            SELECT tablename FROM pg_tables WHERE schemaname = 'public';
+            `);
+
+        const existingTables = results.map((row: any) => row.tablename);
+
+        const models = [LogModel, ReadingModel]; // Novos modelos devem ser adicionados aqui
+                                                 // todo: pensar forma de abstrair onde ficará os modelos existentes do projeto
+        const missingModels = models.filter( model => !existingTables.includes(model.getTableName()));
+
+        if (missingModels.length > 0) {
+            console.log("Some tables are missing, syncing them...");
+            await Promise.all(missingModels.map(model => model.sync()));
+            console.log("Missing tables have been created.");
+        } else {
+            console.log("All tables exists. No sync needed.");
+        }
+    } catch (error) {
+        console.error("Error synchronizing database:", error);
+    } finally {
+        await sequelize.close();
+    }
+};
+
+checkAndSyncDatabase ();
