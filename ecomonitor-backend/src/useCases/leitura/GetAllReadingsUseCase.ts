91ee859cbc6b33b052251d70077457b78ec99bf7
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-17 10:51:53 -0300

    feat: criado user case GetAllReadingsUseCase
    use case para recuperacao de todos os Readings do banco de dados.

---
+++ ecomonitor-backend/src/useCases/leitura/GetAllReadingsUseCase.ts
@@ -0,0 +1,29 @@
+import { Reading } from "../../domain/leitura/reading";
+import { IReadingRepository } from "../../repositories/reading/IReadingRepository";
+import { LogService } from "../../services/LogService";
+import { ILogRepository } from "../../repositories/logs/ILogRepository";
+import { GetAllReadingsDTO } from "../../dtos/reading/GetAllReadingsDTO";
+
+export class GetAllReadingsUseCase {
+    constructor(
+        private readingRepository: IReadingRepository,
+        private logRepository: ILogRepository
+    ) {}
+
+    async execute(data: GetAllReadingsDTO, requestIp: string, userAgent?: string, userId?: string): Promise<Reading[] | null> {
+        const logService = new LogService(this.logRepository);
+
+        try {
+            const readings = await this.readingRepository.findAll();
+            
+            await logService.logGetAllItensSuccess("Reading", "/api/leituras", requestIp, userAgent, userId);
+            
+            if (!readings) return null;
+            return readings;
+        } catch (error: any) {
+            await logService.logGetAllItensError("Reading", "/api/leituras", requestIp, error.stack, userAgent, userId);
+
+            throw error;
+        }
+    }
+}
