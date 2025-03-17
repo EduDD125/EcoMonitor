004b729d80ee2a4c9aefbe37536c7a8cb94f99d5
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-17 01:28:58 -0300

    style: removido espaços em branco no codigo

--- ecomonitor-backend/src/dtos/reading/ReadingResponseDTO.ts
+++ ecomonitor-backend/src/dtos/reading/ReadingResponseDTO.ts
@@ -4,5 +4,4 @@ export interface ReadingResponseDTO {
     dateTime: string;
     measurementType: string;
     value: string;
-  }
-  
+}
--- ecomonitor-backend/src/dtos/reading/UpdateReading.ts
+++ ecomonitor-backend/src/dtos/reading/UpdateReading.ts
@@ -4,5 +4,5 @@ export interface UpdateReadingDTO {
     dateTime?: string;
     measurementType?: string;
     value?: string;
-  }
+}
   
