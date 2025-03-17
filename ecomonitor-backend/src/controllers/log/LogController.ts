5338f0c603c119ae3581585fe8f4b8a0c948e17f
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-17 16:46:11 -0300

    feat: criado controllers para use cases da entidade Log

---
+++ ecomonitor-backend/src/controllers/log/LogController.ts
@@ -0,0 +1,43 @@
+
+import { LogRepositoryPostgreSQL } from "../../repositories/logs/LogRepositoryPostgreSQL";
+import { CreateReadingDTO } from "../../dtos/reading/CreateReadingDTO";
+import { UpdateReadingDTO } from "../../dtos/reading/UpdateReading";
+import { DeleteReadingDTO } from "../../dtos/reading/DeleteReadingDTO";
+import { GetAllLogsUseCase } from "../../useCases/log/GetAllLogsUseCase";
+import { GetLogByIdUseCase } from "../../useCases/log/GetLogByIdUseCase";
+
+export class LogController {
+    private logRepository = new LogRepositoryPostgreSQL();
+
+    private getAllLogsUseCase = new GetAllLogsUseCase(this.logRepository);
+    private getLogByIdUseCase = new GetLogByIdUseCase(this.logRepository);
+
+    async getAll(req: any, res: any) {
+        try {
+            const requestIp = req.ip;
+            const userAgent = req.get("User-Agent") || "";
+
+            const logs = await this.getAllLogsUseCase.execute(requestIp as string, userAgent); // todo: pensar como pegar id de usuário para parametro UserID
+            return res.status(200).json(logs);
+        } catch (error: any) {
+            return res.status(404).json({ error: error.message });
+        }
+    }
+
+    async getById(req: any, res: any) {
+        try {
+            const { id } = req.params;
+            const requestIp = req.ip;
+            const userAgent = req.get("User-Agent") || "";
+
+            const log = await this.getLogByIdUseCase.execute(id, requestIp as string, userAgent); // todo: pensar como pegar id de usuário para parametro UserID
+            if (!log) {
+                return res.status(404).json({ error: `Log with ID ${id} not found` });
+            }
+
+            return res.status(200).json(log);
+        } catch (error: any) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+}
