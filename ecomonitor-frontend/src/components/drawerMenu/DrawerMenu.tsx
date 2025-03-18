bdfd4f7d330cb6c80637f66d5691268d6ae0ef0d
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-18 19:29:11 -0300

    feat: drawer menu recebe acordeons que listam funcionalidades dentro de temas, permitido melhor organização das funcionalidades do sistema

--- ecomonitor-frontend/src/components/drawerMenu/DrawerMenu.tsx
+++ ecomonitor-frontend/src/components/drawerMenu/DrawerMenu.tsx
@@ -1,53 +1,81 @@
 import React, { useState } from "react";
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton } from "@mui/material";
+import { Drawer, List, ListItem, ListItemText, IconButton, Accordion, AccordionSummary, AccordionDetails, ListItemButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Link } from "react-router-dom";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import ListAltIcon from "@mui/icons-material/ListAlt";
+import { Link } from "react-router-dom";
 
-interface IRoutes {
+interface ISubRoute {
   path: string;
   text: string;
+}
+
+interface IRoute {
+  text: string;
   icon: React.JSX.Element;
+  subRoutes: ISubRoute[];
 }
 
 const DrawerMenu: React.FC = () => {
-    const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
 
-    const toggleDrawer = (state: boolean) => () => {
-        setOpen(state);
-    };
+  const toggleDrawer = (state: boolean) => () => {
+    setOpen(state);
+  };
 
-    const drawerMenuRoutes: IRoutes[] = [
-        {
-        path: "/leituras",
-        text: "Leituras",
-        icon: <BarChartIcon />,
-        },
-        {
-        path: "/logs",
-        text: "Logs do Sistema",
-        icon: <ListAltIcon />,
-        },
-    ];
+  const drawerMenuRoutes: IRoute[] = [
+    {
+      text: "Leituras",
+      icon: <BarChartIcon />,
+      subRoutes: [
+        { path: "/leituras/", text: "Registros e Exportação" },
+        { path: "/leituras/estatisticas", text: "Estatísticas" },
+      ],
+    },
+    {
+      text: "Logs do Sistema",
+      icon: <ListAltIcon />,
+      subRoutes: [
+        { path: "/logs/", text: "Registros e Exportação" },
+        { path: "/logs/estatisticas", text: "Estatísticas" },
+      ],
+    },
+  ];
 
-    return (
-        <>
-            <IconButton onClick={toggleDrawer(true)}>
-                <MenuIcon />
-            </IconButton>
-            <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
-                <List>
-                {drawerMenuRoutes.map((route, index) => (
-                    <ListItem key={index} component={Link} to={route.path} onClick={toggleDrawer(false)}>
-                    <ListItemIcon>{route.icon}</ListItemIcon>
-                    <ListItemText primary={route.text} />
-                    </ListItem>
-                ))}
+  return (
+    <>
+      <IconButton onClick={toggleDrawer(true)}>
+        <MenuIcon />
+      </IconButton>
+      <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+        <List>
+          {drawerMenuRoutes.map((route, index) => (
+            <Accordion key={index}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                {route.icon}
+                <ListItemText primary={route.text} />
+              </AccordionSummary>
+              <AccordionDetails>
+                <List component="div" disablePadding>
+                    {route.subRoutes.map((subRoute, subIndex) => (
+                        <ListItemButton
+                            key={subIndex}
+                            component={Link as any}
+                            to={subRoute.path}
+                            onClick={toggleDrawer(false)}
+                        >
+                        <ListItemText primary={subRoute.text} />
+                        </ListItemButton>
+                    ))}
                 </List>
-            </Drawer>
-        </>
-    );
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  );
 };
 
 export default DrawerMenu;
