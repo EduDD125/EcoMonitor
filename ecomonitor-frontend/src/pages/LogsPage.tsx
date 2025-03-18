5d753ac96a1af90683c322dbab782464f22e6c3c
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-18 19:14:41 -0300

    feat: criado páginas da aplicação web
    pagina para autentificação (implementação futura)
    landingPage (implementação futura)
    Pagina de leituras do sistema
    Pagina de logs do sistema

---
+++ ecomonitor-frontend/src/pages/LogsPage.tsx
@@ -0,0 +1,12 @@
+import ControlPainel from "src/components/painels/controlPainel/controlPainel";
+import UpperBanner from "src/components/upperBanner/upperBanner";
+
+export default function LogsPage() {
+    return (
+      <>
+        <ControlPainel>
+          <div></div>
+        </ControlPainel>
+      </>
+    );
+  };
---
+++ ecomonitor-frontend/src/pages/ReadingsPage.tsx
@@ -0,0 +1,11 @@
+import ControlPainel from "../components/painels/controlPainel/controlPainel";
+
+export default function ReadingsPage() {
+  return (
+    <>
+      <ControlPainel>
+        <div></div>
+      </ControlPainel>
+    </>
+  );
+};
