e5c56db5f36fa6ec437e68581e2c119a5d9e046f
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-18 20:04:19 -0300

    feat: hook que abstrai qualquer resgate de todos dos registros de uma entidade para alimentar tabelas.

---
+++ ecomonitor-frontend/src/hooks/useFetchTablesData.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import api from "./../services/api";
+
+const useFetchTablesData = <T,>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await api.get<T[]>(endpoint);
+        setData(response.data);
+      } catch (err) {
+        setError("Erro ao buscar os dados.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [endpoint]);
+
+  return { data, loading, error };
+};
+
+export default useFetchTablesData;
