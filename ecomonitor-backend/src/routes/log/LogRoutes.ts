5ef26c83f4d2350786f24db389b68af1b76dab13
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-17 23:07:34 -0300

    fix: corrigido nome de variavel exportada por arquivos de rotas

--- ecomonitor-backend/src/routes/log/LogRoutes.ts
+++ ecomonitor-backend/src/routes/log/LogRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { LogController } from "../../controllers/log/LogController";
 
-const router = Router();
+const logRouter = Router();
 const logController = new LogController();
 
-router.get("/api/logs", (req, res) => logController.getAll(req, res));
-router.get("/api/logs/:id", (req, res) => logController.getById(req, res));
+logRouter.get("/api/logs", (req, res) => logController.getAll(req, res));
+logRouter.get("/api/logs/:id", (req, res) => logController.getById(req, res));
 
-export default router;
+export default logRouter;
 
