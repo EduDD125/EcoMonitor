6e3460947fe30b180869a175774e4f2de63013cf
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-18 19:11:52 -0300

    feat: criado componente banner no topo do painel de controle

---
+++ ecomonitor-frontend/src/components/upperBanner/upperBanner.tsx
@@ -0,0 +1,9 @@
+import "./upperBannerStyle.css"
+
+export default function UpperBanner() {
+    return (
+        <div className="upper-banner__container">
+            <img src={"/photos/logos/EcoMONITOR-log.png"}/>
+        </div>
+    );
+}
