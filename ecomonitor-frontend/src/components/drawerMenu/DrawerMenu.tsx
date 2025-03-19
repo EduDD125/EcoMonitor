daf3f419abb92c20487d05dcf56cac89677d5c97
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-18 23:11:45 -0300

    feat: novo estilo de drawer

--- ecomonitor-frontend/src/components/drawerMenu/DrawerMenu.tsx
+++ ecomonitor-frontend/src/components/drawerMenu/DrawerMenu.tsx
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
-import { Drawer, List, ListItem, ListItemText, IconButton, Accordion, AccordionSummary, AccordionDetails, ListItemButton } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  ListItemButton,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import BarChartIcon from "@mui/icons-material/BarChart";
@@ -18,11 +28,21 @@ interface IRoute {
 }
 
 const DrawerMenu: React.FC = () => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [expanded, setExpanded] = useState<boolean>(false);
 
-  const toggleDrawer = (state: boolean) => () => {
-    setOpen(state);
+  const toggleExpand = () => {
+    if (expanded){
+      console.log("2");
+       setExpanded((prev) => !prev);
+      }
   };
+
+  const boxToggleExpand = () => {
+    if (!expanded){
+      console.log("2");
+       setExpanded((prev) => !prev);
+      }
+  }
 
   const drawerMenuRoutes: IRoute[] = [
     {
@@ -44,42 +64,54 @@ const DrawerMenu: React.FC = () => {
   ];
 
   return (
-    <>
-      <IconButton onClick={toggleDrawer(true)} sx={{display: "flex", flexDirection: "column", height: "100%", justifyContent: "start"}}>
-        <MenuIcon sx={{marginBottom: "20px"}}/>
-        {drawerMenuRoutes.map((item, index) => (
-          <div key={index}>
-            {item.icon}
-          </div>
-        ))}
+    <Box sx={{ 
+      display: "flex", 
+      flexDirection: "column", 
+      width: expanded ? 240 : 60, 
+      transition: "width 0.3s",
+      height: "100%",
+      }}
+    onClick={boxToggleExpand}
+    >
+      <IconButton onClick={toggleExpand} sx={{ alignSelf: "center", marginBottom: 2 }}>
+        <MenuIcon />
       </IconButton>
-      <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
-        <List>
-          {drawerMenuRoutes.map((route, index) => (
-            <Accordion key={index}>
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                {route.icon}
-                <ListItemText primary={route.text} />
-              </AccordionSummary>
-              <AccordionDetails>
-                <List component="div" disablePadding>
+      <List>
+        {drawerMenuRoutes.map((route, index) => (
+          <Box key={index}>
+            {expanded ?
+              <Accordion disableGutters>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <IconButton>
+                    {route.icon}
+                  </IconButton>
+                  <ListItemText primary={route.text} />
+                </AccordionSummary>
+                <AccordionDetails>
+                  <List component="div" disablePadding>
                     {route.subRoutes.map((subRoute, subIndex) => (
-                        <ListItemButton
-                            key={subIndex}
-                            component={Link as any}
-                            to={subRoute.path}
-                            onClick={toggleDrawer(false)}
-                        >
+                      <ListItemButton
+                        key={subIndex}
+                        component={Link}
+                        to={subRoute.path}
+                      >
                         <ListItemText primary={subRoute.text} />
-                        </ListItemButton>
+                      </ListItemButton>
                     ))}
-                </List>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </List>
-      </Drawer>
-    </>
+                  </List>
+                </AccordionDetails>
+              </Accordion>
+              :
+              <ListItem disablePadding sx={{justifyContent: "center"}}>
+                <IconButton>
+                  {route.icon}
+                </IconButton>
+              </ListItem>
+             }
+          </Box>
+        ))}
+      </List>
+    </Box>
   );
 };
 
