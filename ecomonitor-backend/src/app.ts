cd08233c03eb80e8a91e2470738ac8a5d179e199
Eduardo Derisso <dudu.derisso@gmail.com>
2025-03-18 21:25:22 -0300

    feat: adicionado COR a aplicação

--- ecomonitor-backend/src/app.ts
+++ ecomonitor-backend/src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import readingRouter from './routes/reading/ReadingRoutes';
 import logRouter from './routes/log/LogRoutes';
 import dotenv from 'dotenv';
+import corsMiddleware from './middlewares/cors';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(corsMiddleware);
 app.use(readingRouter);
 app.use(logRouter);
 
